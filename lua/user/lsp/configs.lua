77c531fceaab8c1ea983fe7ec12c3f442e579a9d
nullptr <3621629+0x0L@users.noreply.github.com>
2023-02-19 22:51:39 +0100

    switch to mason

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -1,13 +1,13 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-	return
-end
+--local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+--if not status_ok then
+--	return
+--end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "pyright", "cmake", "ccls", "jsonls", "sumneko_lua" }
+local servers = { "pyright", "cmake", "ccls", "jsonls", "lua_ls" }
 
-lsp_installer.setup({
+require("mason").setup({
 	ensure_installed = servers,
 })
 
